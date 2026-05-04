7166e210f85f9e90fe9e6768f34b303f70d343b7
elnuraErkinbekova <elnuraerkinbekova8@gmail.com>
2026-05-05 01:36:12 +0600

    added first logic: cards opening at the start

--- logic.js
+++ logic.js
@@ -1,7 +1,144 @@
+// make deck
+const suits = ["H", "D", "C", "S"];
+const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"];
+
+let deck = [];
+
+for (let suit of suits) {
+	for (let rank of ranks) {
+		deck.push({
+			type: "card",
+			suit: suit,
+			rank: rank,
+		});
+	}
+}
+
+deck.push({ type: "Joker" }, { type: "Joker" });
+
+function shuffleDeck(deck) {
+	for (let i = deck.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[deck[i], deck[j]] = [deck[j], deck[i]];
+	}
+	return deck;
+}
+
+let shuffledDeck = shuffleDeck(deck);
+console.log(shuffledDeck);
+
+// make player and computer cards
+
+let computer_cards = [];
+let player_cards = [];
+
+for (let i = 0; i < 4; i++) {
+	computer_cards.push(shuffledDeck.pop());
+	player_cards.push(shuffledDeck.pop());
+}
+
+console.log(computer_cards);
+console.log(player_cards);
+
+console.log(shuffledDeck);
+
+// start and stop buttons
 const start_btn = document.getElementById("start_btn");
 const stop_btn = document.getElementById("stop_btn");
 
 function start() {
 	start_btn.style.display = "none";
 	stop_btn.style.display = "inline-block";
+
+	showTwoCards();
+}
+
+// function openModal() {
+// 	document.getElementById("modal").classList.remove("hidden");
+// }
+
+// function closeModal() {
+// 	document.getElementById("modal").classList.add("hidden");
+// }
+
+// assign cards
+const c1 = document.getElementById("c1");
+const c2 = document.getElementById("c2");
+const c3 = document.getElementById("c3");
+const c4 = document.getElementById("c4");
+
+const p1 = document.getElementById("p1");
+const p2 = document.getElementById("p2");
+const p3 = document.getElementById("p3");
+const p4 = document.getElementById("p4");
+
+let c_card1 = computer_cards[0];
+let c_card2 = computer_cards[1];
+let c_card3 = computer_cards[2];
+let c_card4 = computer_cards[3];
+
+let p_card1 = player_cards[0];
+let p_card2 = player_cards[1];
+let p_card3 = player_cards[2];
+let p_card4 = player_cards[3];
+
+// show first two cards
+function showTwoCards() {
+	if (p_card3.type === "card") {
+		p3.src = `./cards/${p_card3.rank}${p_card3.suit}.svg`;
+	} else {
+		p3.src = "./cards/Joker.svg";
+	}
+
+	if (p_card4.type === "card") {
+		p4.src = `./cards/${p_card4.rank}${p_card4.suit}.svg`;
+	} else {
+		p4.src = "./cards/Joker.svg";
+	}
+
+	p3.style.position = "relative";
+	p3.style.zIndex = "10000";
+	p4.style.position = "relative";
+	p4.style.zIndex = "10000";
+	p3.classList.add("card-highlight");
+	p4.classList.add("card-highlight");
+
+	startCountdown(5);
+
+	setTimeout(() => {
+		closeTwoCards();
+		p3.style.position = "";
+		p3.style.zIndex = "";
+		p4.style.position = "";
+		p4.style.zIndex = "";
+		p3.classList.remove("card-highlight");
+		p4.classList.remove("card-highlight");
+	}, 5000);
+}
+
+// close them
+function closeTwoCards() {
+	p3.src = "./cards/back.svg";
+	p4.src = "./cards/back.svg";
+}
+
+//countdown modal
+let countdownInterval;
+
+function startCountdown(seconds) {
+	let timeLeft = seconds;
+
+	document.getElementById("countdown_modal").classList.remove("hidden");
+	document.getElementById("countdown_text").innerText = timeLeft;
+
+	countdownInterval = setInterval(() => {
+		timeLeft--;
+
+		document.getElementById("countdown_text").innerText = timeLeft;
+
+		if (timeLeft <= 0) {
+			clearInterval(countdownInterval);
+			document.getElementById("countdown_modal").classList.add("hidden");
+		}
+	}, 1000);
 }
