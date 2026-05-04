a51f508e95f1d34da609e2d088ccc7b35fe5d069
elnuraErkinbekova <elnuraerkinbekova8@gmail.com>
2026-05-05 00:02:36 +0600

    add buttons

---
+++ logic.js
@@ -0,0 +1,7 @@
+const start_btn = document.getElementById("start_btn");
+const stop_btn = document.getElementById("stop_btn");
+
+function start() {
+	start_btn.style.display = "none";
+	stop_btn.style.display = "inline-block";
+}
